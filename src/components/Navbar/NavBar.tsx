90d3e17018c2cafa5a73e89c3cdce6e6b0c65ca6
guiligancjg <guiligan7@gmail.com>
2023-11-25 17:24:09 -0300

    Se actuliazo el logo a png8

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -49,7 +49,7 @@ const NavBar = () => {
         <>
         <Navbar id="detalle-section" expand="lg" className="bg-body-tertiary">
             <Container>
-                <Nav.Link onClick={() => navigate('/')}><Image src={"https://gramajo-carlos-jorge-frond-end.vercel.app/public/imagenes/logo.png"} alt="Gramajo Carlos Jorge Comisión 075" /> </Nav.Link>
+                <Nav.Link onClick={() => navigate('/')}><Image src={"imagenes/logo.png"} alt="Gramajo Carlos Jorge Comisión 075" /> </Nav.Link>
                 
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
